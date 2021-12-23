329839d4886361b0dc31d7cb26d5f1a1dd5f645e
SongMinJun <dkaskgkdua@gmail.com>
2021-12-24 00:31:04 +0900

    1. 타입과 인터페이스 차이 적용

---
+++ class-note/4_type-aliases.ts
@@ -0,0 +1,21 @@
+interface Person {
+    name: string;
+    age: number;
+}
+
+// type Person = {
+//     name: string;
+//     age: number;
+// }
+
+var seho: Person ={
+    name: '민준',
+    age: 29
+}
+type MyString = string;
+var str: MyString = 'Hello';
+
+type Todo = {id: string, title: string, done: boolean};
+function getTodo(todo: Todo) {
+
+}
